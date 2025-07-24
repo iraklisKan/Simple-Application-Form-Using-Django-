e4ca2e0471a4d5646db6216597d1947fbec07ded
Iraklis Kanarkotis <irakliskanbusiness@gmail.com>
2025-07-25 01:57:23 +0300

    Add Railway deployment configuration

--- myproject/settings.py
+++ myproject/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8a($l^p7zw#67vnf216$+e$%+!!-gf^s)$11j+cf&3888dkf8n'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-8a($l^p7zw#67vnf216$+e$%+!!-gf^s)$11j+cf&3888dkf8n')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.railway.app']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
